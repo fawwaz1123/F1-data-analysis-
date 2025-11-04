530dff0507aff2d7be1f9653407d8e97501afb39
Fawwaz Ahmed <fawwazhahmed@gmail.com>
2025-11-04 23:21:56 +0000

    testing relating tables and extracting lap time data

--- plotting.py
+++ plotting.py
@@ -1,10 +1,18 @@
 import matplotlib.pyplot as plt 
 import numpy as np
+import pandas as pd
 
-x = np.array([0,6])
-y = np.array([0,250])
+laptimes= pd.read_csv('lap_times.csv')
+drivers = pd.read_csv('drivers.csv')
+times = laptimes.query('raceId == 841')[['lap','time']]
+driverLap_times = pd.merge(drivers,laptimes,how = 'inner', on= 'driverId')
+hamilton_times = driverLap_times.query("(driverRef == 'hamilton') and (raceId == 841)")
 
-print(x,y)
+print(times)
+print(laptimes.columns)
+print(driverLap_times)
+print(hamilton_times)
 
-plt.plot(x,y)
+
+hamilton_times[['lap','time']].plot(x='time',y='lap')
 plt.show()
